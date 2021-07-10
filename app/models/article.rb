3868a25fa9cba140009f68c2918d24fe6738706d
Yvette Tan <yvettetan@Yvettes-MacBook-Pro.local>
2021-07-10 21:12:33 +0800

    Adds validation to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true,
+                    uniqueness: true,
+  validates :body, presence: true,
+                   length: { minimum: 5 } 
 end
