59ec943af7284d74a857d341cb7212f88ba7916c
Yvette Tan <yvettetan97@gmail.com>
2021-07-15 17:52:47 +0800

    Adds CRUD for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true,
-                    uniqueness: true,
-  validates :body, presence: true,
-                   length: { minimum: 5 } 
+  validates :title, { presence: true, uniqueness: true }
+  validates :body, { presence: true, length: { minimum: 5 } }
 end
