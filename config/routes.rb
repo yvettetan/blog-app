e49549ef8ad402debc202925cf197e1433b45d44
Yvette Tan <yvettetan97@gmail.com>
2021-07-16 14:49:27 +0800

    Adds home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'articles#index'
 
   resources :articles
 end
