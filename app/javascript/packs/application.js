b06e97110a4bc2a21b6829fe3c6dbf0dcbd08482
Yvette Tan <yvettetan97@gmail.com>
2021-07-18 02:06:05 +0800

    Installs action text wysiwyg editor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,3 +25,5 @@ document.addEventListener("DOMContentLoaded", function (event) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
 });
+require("trix")
+require("@rails/actiontext")
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, { presence: true, uniqueness: true }
   validates :body, { presence: true, length: { minimum: 5 } }
+  has_rich_text :body
 end
