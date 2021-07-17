e8b9f704ad2e41fdb116c4f62963b3b064f52c30
Yvette Tan <yvettetan97@gmail.com>
2021-07-17 23:26:35 +0800

    Removes bootstrap gem and installs bootstrap with yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,17 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-//= require jquery3
-//= require popper
-//=require bootstrap-sprockets
+import * as bootstrap from 'bootstrap'
+import "../stylesheets/application"
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
