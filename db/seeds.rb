e87ab2e7b018b31fd191e13f4ac8de8f4c9cbef4
Yvette Tan <yvettetan97@gmail.com>
2021-07-21 01:16:48 +0800

    Creates admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'user@example.com',
+             password: 'password',
+             password_confirmation: 'password',
+             admin: true)
