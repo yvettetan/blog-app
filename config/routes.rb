d53afa34ca934070b763cce2717bf15500e59625
Yvette Tan <yvettetan97@gmail.com>
2021-07-21 01:11:10 +0800

    Creates user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
   get 'home/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :articles
 end
