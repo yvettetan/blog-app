02b171c053dae839e5daf98d9246df07f8dac473
Yvette Tan <yvettetan97@gmail.com>
2021-07-21 14:17:56 +0800

    Creates helper method for authenticating admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # #helper method to make sure user is not admin to be sent back to home page
+  helper_method :is_admin!
+
+  private
+
+  def is_admin!
+    # #if user is logged in and if user is an admin
+    if current_user&.admin
+      nil
+    else
+      redirect_to root_path
+    end
+  end
 end
