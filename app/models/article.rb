61b13bc9b3aef3cafcd66e8879807e1e88f5f121
Yvette Tan <yvettetan97@gmail.com>
2021-07-21 18:04:21 +0800

    Adds attachments to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,18 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
+  # Active Storage
+  has_one_attached :thumbnail
+  has_one_attached :banner
+
+  # Action Text from rails 6
+  has_rich_text :body
+
   validates :title, { presence: true, uniqueness: true }
   validates :body, { presence: true, length: { minimum: 5 } }
-  has_rich_text :body
+
+  # method to resize images
+  def optmized_image(image, width, height)
+    image.variant(resize_to_fill: [width, height]).processed
+  end
 end
