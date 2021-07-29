cb54a1af15fc9e2e393bbffb5823c3cff3b3b51d
Yvette Tan <yvettetan97@gmail.com>
2021-07-29 16:49:47 +0800

    Creates comments for each article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :commenter, :body, presence: true
 end
