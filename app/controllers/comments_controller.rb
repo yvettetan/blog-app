ca938d539be65bcef0340374d67e4d00d6826b0d
yvettetan <yvettetan97@gmail.com>
2021-08-10 15:21:48 +0800

    Adds destroy action for comments and authenticates user before creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
