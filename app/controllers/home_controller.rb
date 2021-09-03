4b81e9353fe1dd86cc2dd0e8c0a8ece50664a21f
yvettetan <yvettetan97@gmail.com>
2021-09-03 11:16:06 +0800

    Adds random feature articles in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @article_feature1 = Article.last
     @article_feature2 = Article.second_to_last
+    @feature_articles = Article.all.shuffle.slice(0, 2)
   end
 end
