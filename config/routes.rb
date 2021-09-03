903bc3e65f0cd02d8c197a1008b35bb190a5526f
yvettetan <yvettetan97@gmail.com>
2021-09-03 11:14:27 +0800

    Removes route to about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  get 'home/about'
   # creates comments as a nested resource within articles
   resources :articles do
     resources :comments
